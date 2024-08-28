729f6d737c0b398a6e5506dd48c41b8ffcebb571
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2024-08-28 23:48:45 +0530

    Sort an array of 0's 1's & 2's.java

--- Sort an array of 0's 1's & 2's.java
+++ Sort an array of 0's 1's & 2's.java
@@ -80,6 +80,23 @@ Space complexity :O(n)
   
 // ************************************************************************************* Better Solution *********************************************************************************
 
+    /* 
+ Dutch National Flag Algorithm 
+
+Explanation : The idea is to sort the array of size N using three pointers: lo = 0, mid = 0 and hi = N – 1 such that the array is divided into three parts:
+
+arr[0] to arr[lo – 1]: This part will have all the zeros.
+arr[lo] to arr[mid – 1]: This part will have all the ones.
+arr[hi + 1] to arr[N – 1]: This part will have all the twos.
+Traverse over the array till mid <= hi, according to the value of arr[mid] we can have three cases:
+
+arr[mid] = 0, then swap arr[lo] and arr[mid] and increment lo by 1 because all the zeros are till index lo – 1 and move to the next element so increment mid by 1.
+arr[mid] = 1, then move to the next element so increment mid by 1.
+arr[mid] = 2, then swap arr[mid] and arr[hi] and decrement hi by 1 because all the twos are from index hi + 1 to N – 1. Now, we don’t move to the next element because the element which is now at index mid can be a 0 and therefore needs to be checked again.
+
+ 
+*/
+
   import java.util.* ;
 import java.io.*; 
 public class Solution {
@@ -132,5 +149,51 @@ Space complexity :O(1)
 
 
   // ************************************************************************************* Optimal Solution *********************************************************************************
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        int low=0;
+        int mid=0;
+        int high=arr.size()-1;
+        while(mid<=high)
+        {
+            if(arr.get(mid)==0)
+            {
+                int temp=arr.get(low);
+                arr.set(low,arr.get(mid));
+                arr.set(mid,temp);
+                mid++;
+                low++;
+
+            }
+            else if(arr.get(mid)==1)
+            {
+                mid++;
+            }
+            else if(arr.get(mid)==2)
+            {
+                
+                int temp=arr.get(high);
+                arr.set(high,arr.get(mid));
+                arr.set(mid,temp);
+                high--;
+
+            }
+        }
+
+
+
+     
+
+    }
+}
+
+
+Time complexity : O(n)
+Space complexity :O(1)
+
+
 
 
