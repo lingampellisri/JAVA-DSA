58a5b26f2e578fdd996b7157250b3b90eb1b01ec
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2024-09-28 19:22:24 +0530

    Create number of occurrences of 'x' in the array.java

---
+++ number of occurrences of 'x' in the array.java
@@ -0,0 +1,140 @@
+/*
+Problem statement
+You have been given a sorted array/list of integers 'arr' of size 'n' and an integer 'x'.
+
+
+
+Find the total number of occurrences of 'x' in the array/list.
+
+
+
+Example:
+Input: 'n' = 7, 'x' = 3
+'arr' = [1, 1, 1, 2, 2, 3, 3]
+
+Output: 2
+
+Explanation: Total occurrences of '3' in the array 'arr' is 2.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+7 3
+1 1 1 2 2 3 3
+
+
+Sample Output 1:
+2
+
+
+Explanation For Sample Input 1:
+In the given list, there are 2 occurrences of integer 3.
+
+
+Sample Input 2:
+ 5 6
+ 1 2 4 4 5
+
+
+Sample Output 2:
+ 0
+
+
+Explanation For Sample Input 2:
+In the given list, there are 0 occurrences of integer 6.
+
+
+Expected time complexity:
+The expected time complexity is O(log 'n').
+
+
+Constraints:
+1 <= n <= 10^4
+1 <= arr[i] <= 10^9
+1 <= x <= 10^9
+Where arr[i] represents the element i-th element in the array/list.
+
+Time Limit: 1sec
+
+*/
+
+
+public class Solution {
+
+    public static int first(int arr[],int n,int x)
+    {
+        int first=-1;
+            int left=0;
+            int right=n-1;
+      while(left<=right)
+            {
+                int mid=(left+right)/2;
+                if(arr[mid]==x)
+                {
+                    first=mid;
+                    right=mid-1;;
+                }
+                
+                else if(x<arr[mid])
+                {
+                
+                    right=mid-1;;
+                }
+                else
+                {
+                    left=mid+1;
+                }
+                
+            }
+        return first;
+    }
+
+
+      public static int last(int arr[],int n,int x)
+    {
+        int last=-1;
+            int left=0;
+            int right=n-1;
+      while(left<=right)
+            {
+                int mid=(left+right)/2;
+                if(arr[mid]==x)
+                {
+                    last=mid;
+                    left=mid+1;
+                }
+                
+                else if(x<arr[mid])
+                {
+                
+                    right=mid-1;;
+                }
+                else
+                {
+                    left=mid+1;
+                }
+                
+            }
+        return last;
+    }
+
+
+
+
+
+    public static int count(int arr[], int n, int x) {
+        //Your code goes here
+
+        int first= first(arr,n,x);
+        if(first==-1)
+        return 0;
+
+        int last=last(arr,n,x);
+        return last-first+1;
+        
+
+    }
+}
+
+
+
