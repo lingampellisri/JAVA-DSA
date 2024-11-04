a4ba0d2e1b2f09910917c4bfb376e811d48164b1
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2024-11-04 18:14:45 +0530

    Update Integer_to_binary_number.java

--- Bit manipulation/Integer_to_binary_number.java
+++ Bit manipulation/Integer_to_binary_number.java
@@ -11,25 +11,36 @@ public class bitManipulation {
 		System.out.println("Enter the number : ");
 		int num=sc.nextInt();
 		int num2=num;
-		StringBuilder sb=new StringBuilder();
-		
-		while(num!=1)
+		if(num<0)
 		{
-			if(num%2==0)
+			System.out.println("please enter a positive number");
+			
+		}
+		else if(num==0)
+		{
+			System.out.println(num +" its binary equivalent is : 0");
+		}
+		else
+		{
+			StringBuilder sb=new StringBuilder();
+			
+			while(num>0)
 			{
-				sb.insert(0,'0');
-			}
-			else
-			{
-				sb.insert(0,'1');
+				if(num%2==0)
+				{
+					sb.insert(0,'0');
+				}
+				else
+				{
+					sb.insert(0,'1');
+				}
+				
+				num=num/2;
 			}
 			
-			num=num/2;
+			System.out.println("Integer :"+num2+" to it Binary Equivalent :"+sb);
+			
 		}
-		if(num==1)
-			sb.insert(0, '1');
-		System.out.println("Integer :"+num2+" to it Binary Equivalent :"+sb);
-		
 
 	}
 
