f17cc09a75f9d24d4d4c2f5f2034d6ce0c2ac317
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2024-11-19 14:12:48 +0530

    Create Binary Tree Level Order Traversal.java

---
+++ Trees/Binary Tree Level Order Traversal.java
@@ -0,0 +1,83 @@
+/*
+102. Binary Tree Level Order Traversal
+Solved
+Medium
+Topics
+Companies
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+List<List<Integer>> result=new LinkedList<List<Integer>>();
+    Queue<TreeNode> queue=new LinkedList<>();
+        if(root==null)
+        {
+            return result;
+        }
+    queue.offer(root);
+    while(!queue.isEmpty())
+    {
+        int len=queue.size();
+        List<Integer>sub=new LinkedList<Integer>();
+
+        for(int i=0;i<len;i++)
+        {
+            if(queue.peek().left!=null)
+            {
+                    queue.offer(queue.peek().left);
+            }
+            if(queue.peek().right!=null)
+            {
+                queue.offer(queue.peek().right);
+            }
+                sub.add(queue.poll().val);
+
+        }
+
+        result.add(sub);
+
+    }
+return result;
+
+        
+    }
+}
