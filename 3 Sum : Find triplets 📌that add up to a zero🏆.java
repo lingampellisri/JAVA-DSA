36735360c37a4d5fc43bfd55c97523dce76442d5
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2025-02-14 21:26:47 +0530

    Update 3 Sum : Find triplets 📌that add up to a zero🏆.java

--- 3 Sum : Find triplets 📌that add up to a zero🏆.java
+++ 3 Sum : Find triplets 📌that add up to a zero🏆.java
@@ -121,3 +121,59 @@ Space Complexity :O(n)
 
   ***************************************************************************Optimal solution***********************************************************************
   
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res=new ArrayList<>();
+
+        Arrays.sort(nums);
+        int n=nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            
+            int j=i+1;
+            int k=n-1;
+
+            if(i>0 && nums[i]==nums[i-1])
+            {
+                continue;
+            }
+
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
+                    j++;
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                else
+                {
+                    List<Integer>ls=new ArrayList<>();
+                    ls.add(nums[i]);
+                    ls.add(nums[j]);
+                    ls.add(nums[k]);
+                    res.add(ls);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                    {
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1])
+                    {
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return res;
+        
+        
+    }
+}
