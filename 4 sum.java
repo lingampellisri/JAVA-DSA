16bcd878a188ef81879aba0195284e9600ecd86e
Srinivas lingampelli <146204265+lingampellisri@users.noreply.github.com>
2025-02-14 23:50:41 +0530

    Update 4 sum.java

--- 4 sum.java
+++ 4 sum.java
@@ -152,3 +152,70 @@ Space Complexity : O(n)
 
 
 **************************************************************************Optimal solution***********************************************************************
+
+
+    class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n=nums.length;
+    List<List<Integer>> res=new ArrayList<>();
+    Arrays.sort(nums);
+    for(int i=0;i<n-3;i++)
+    {
+
+        if(i>0 && nums[i]==nums[i-1])
+        {
+            continue;
+        }
+
+        for(int j=i+1;j<n-2;j++)
+        {
+            if(j>(i+1) && nums[j]==nums[j-1])
+            {
+                continue;
+            }
+
+            int k=j+1;
+            int l=n-1;
+
+            while(k<l)
+            {
+                long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+
+                if(sum==target)
+                {
+                    List<Integer>ls=new ArrayList<>();
+                    ls.add(nums[i]);
+                    ls.add(nums[j]);
+                    ls.add(nums[k]);
+                    ls.add(nums[l]);
+                    res.add(ls);
+                    k++;
+                    l--;
+
+                    while(k<l && nums[k]==nums[k-1])
+                    {
+                        k++;
+                    }
+                    while(k<l && nums[l]==nums[l+1])
+                    {
+                        l--;
+                    }
+                }
+                else if(sum<target)
+                {
+                    k++;
+                }
+                else
+                {
+                    l--;
+                }
+            }
+
+
+        }
+    }
+
+        return res;
+        
+    }
+}
